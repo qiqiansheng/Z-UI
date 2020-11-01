af29fd1b4fdf21012f6a3db555fe485aa8834e09
liuwenzhuo <13880453494@qq.com>
2020-11-01 22:08:15 +0800

    完成Input说明文档

---
+++ examples/components/InputDemo3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <z-input v-model="foo"></z-input>
+    <p>{{ foo }}</p>
+  </div>
+</template>
+
+<script>
+import {zInput} from '../../package'
+export default {
+  data() {
+    return {
+      foo:''
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
--- examples/docs/InputDoc.vue
+++ examples/docs/InputDoc.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="InputDoc">
     <demo :component='InputDemo' title="基础用法" description='Input基础用法' :code='code0'/>
-    <demo :component='InputDemo1' title="可清除" description='添加 clearable 属性设置Input可清除' :code='code1'/>
-    <demo :component='InputDemo2' title="基础用法" description='Input基础用法' :code='code2'/>
-
+    <demo :component='InputDemo1' title="可清空" description='添加 clearable 属性设置Input可清空' :code='code1'/>
+    <demo :component='InputDemo2' title="文本域" description='设置 type 属性' :code='code2'/>
+    <demo :component='InputDemo3' title="支持v-model" description='Input支持 v-model 双向绑定' :code='code3'/>
+    <attribution :data='data'/>
   </div>
 </template>
 
@@ -11,6 +12,7 @@
 import InputDemo from '../components/InputDemo'
 import InputDemo1 from '../components/InputDemo1'
 import InputDemo2 from '../components/InputDemo2'
+import InputDemo3 from '../components/InputDemo3'
 import demo from '../components/demo'
 import attribution from '../components/attribution';
 export default {
@@ -18,16 +20,37 @@ export default {
     demo,
     InputDemo,
     InputDemo1,
-    InputDemo2
+    InputDemo2,
+    InputDemo3,
+    attribution
   },
   data() {
     return {
       code0:`<z-input></z-input>`,
       code1:`<z-input clearable></z-input>`,
       code2:`<z-input type='textarea'></z-input>`,
+      code3:`<z-input v-model="foo"></z-input>
+<p>{{ foo }}</p>`,
       InputDemo,
       InputDemo1,
-      InputDemo2
+      InputDemo2,
+      InputDemo3,
+      data:[
+        {
+          params: 'type',
+          desc: '类型',
+          type: 'String',
+          select: 'text , textarea',
+          default: 'text',
+        },
+        {
+          params: 'clearable',
+          desc: '可清空',
+          type: 'Boolean',
+          select: 'true , false',
+          default: 'false',
+        }
+      ]
     }
   },
 }
